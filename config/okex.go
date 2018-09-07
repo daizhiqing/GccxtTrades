16e914383761f899f53c82e812edf6844fc9be00
DaiZhiqing <daizhiqing8@163.com>
2018-09-07 16:47:42 +0800

    [ADD] Okex

--- config/okex.go
+++ config/okex.go
@@ -6,8 +6,8 @@ const (
 
 
 	OkexOrigin = "https://real.okex.com"
-	//OkexWsUrl =  "wss://real.okex.com:10441/websocket"  //线上
-	OkexWsUrl =  "wss://real.okcoin.com:10440/websocket"  //开发本地
+	OkexWsUrl =  "wss://real.okex.com:10441/websocket"  //线上
+	//OkexWsUrl =  "wss://real.okcoin.com:10440/websocket"  //开发本地
 
 	//获取所有交易对
 	OkexSymbols = "https://www.okex.com/v2/spot/markets/products"
